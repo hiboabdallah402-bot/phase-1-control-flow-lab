f07bb3e8726dfa85a960068fe5ddf8a4486dc711
hibo <hiboabdallah402@gmail.com>
2026-01-07 04:57:17 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  let result;
+  if (distanceInFeet <= 400 ){
+      return  "This one is on me!";
+    } else if (distanceInFeet >= 400 && distanceInFeet <=2000){
+      return  "That will be twenty bucks.";
+    }else if (distanceInFeet > 2000 && distanceInFeet <=2500){
+      return "I will gladly take your thirty bucks.";
+    }else {
+      return "No can do.";
+    }
+    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  {
+ return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+  
+}
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye.";
+  }
+
+
 }
